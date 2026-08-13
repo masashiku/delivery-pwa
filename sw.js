e32fab42e7daae7e3caa6a58487cf73d7af09071
cyanta <144873404+masashiku@users.noreply.github.com>
2026-08-13 19:56:36 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dpwa-v1-1';
+const CACHE_NAME = 'dpwa-v1-2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
